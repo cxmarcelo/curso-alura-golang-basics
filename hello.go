2bbef23f6a58eac14be5d521d70dd455becc6df3
cxmarcelo <marcelo.c.b22@gmail.com>
2023-03-22 23:05:30 -0300

    arrays/slices monitoring x sites.

--- hello.go
+++ hello.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+const monitoringTimes = 3
+const monitoringDealy = 5
 
 func main() {
 	showIntroduction()
@@ -46,20 +50,35 @@ func readCommand() int {
 	var command int
 	fmt.Scan(&command)
 	fmt.Println("O comando escolhido foi", command)
+	fmt.Println("")
 
 	return command
 }
 
 func startMonitoring() {
 	fmt.Println("Monitorando...")
-	site := "http://www.alura.com.br"
+	sites := []string{"http://www.alura.com.br", "http://www.caelum.com.br", "http://www.google.com"}
 
+	for i := 0; i < monitoringTimes; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testSite(site)
+		}
+
+		fmt.Println("")
+		time.Sleep(monitoringDealy * time.Second)
+	}
+
+	fmt.Println("")
+
+}
+
+func testSite(site string) {
 	resp, _ := http.Get(site)
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "Foi carregado com sucesso!")
 	} else {
 		fmt.Println("Site:", site, "Está com problemas. Status code:", resp.StatusCode)
-
 	}
 }
